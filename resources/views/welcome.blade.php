70adf909c4e57dbddfa030d4c1c1eb6012f32a0c
Devendra Kumar <77403879+devendrakumar09@users.noreply.github.com>
2022-05-20 18:21:06 +0530

    Scheduling Queued Jobs

--- resources/views/welcome.blade.php
+++ resources/views/welcome.blade.php
@@ -7,12 +7,20 @@
     <title>Good Morning Application</title>
     <link rel="stylesheet" href="{{ asset('css/app.css') }}">
 </head>
-<body>
-    <div class="container-flude">
-        <div class="container">
-            <h4 class="h4 fw-bolder">welcme To Crone Jobs</h4>
+<body class="mt-4">
+    <div class="container">
+        <div class="row align-items-md-stretch">
+            <div class="col-md-12">
+              <div class="h-100 p-5 text-white bg-dark rounded-3">
+                <h2>Scheduling Queued Jobs</h2>
+                <p>The job method may be used to schedule a queued job. This method provides a convenient way to schedule queued jobs without using the call method to define closures to queue the job:</p>
+                <a href="https://laravel.com/docs/8.x/scheduling#scheduling-queued-jobs" class="btn btn-outline-light" type="button">Explore More</a>
+              </div>
+            </div>
+
         </div>
     </div>
+
 
     <script src="{{ asset('js/app.js') }}"></script>
 </body>
