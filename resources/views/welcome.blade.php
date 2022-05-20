c80f7e45eaf021e9d752e6b286586a75570e9c62
Devendra Kumar <77403879+devendrakumar09@users.noreply.github.com>
2022-05-20 17:26:32 +0530

    Morning Email

---
+++ resources/views/welcome.blade.php
@@ -0,0 +1,19 @@
+<!DOCTYPE html>
+<html lang="en">
+<head>
+    <meta charset="UTF-8">
+    <meta name="viewport" content="width=device-width, initial-scale=1.0">
+    <meta http-equiv="X-UA-Compatible" content="ie=edge">
+    <title>Good Morning Application</title>
+    <link rel="stylesheet" href="{{ asset('css/app.css') }}">
+</head>
+<body>
+    <div class="container-flude">
+        <div class="container">
+            <h4 class="h4 fw-bolder">welcme To Crone Jobs</h4>
+        </div>
+    </div>
+
+    <script src="{{ asset('js/app.js') }}"></script>
+</body>
+</html>
